42f3ae065d8e4ec864739525550480ad0ed32fc3
Stéphane Conq <conqstephane@gmail.com>
2020-07-19 14:48:31 +0200

    updated guard

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import {AuthService} from "../_services/auth.service";
 
@@ -8,13 +8,23 @@ import {AuthService} from "../_services/auth.service";
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService) {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
   }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.authService.qAuth;
+    return new Promise(resolve => {
+      this.authService.qAuth.then(authorized => {
+        if (!authorized) {
+          this.router.navigate(['login']);
+        }
+        resolve(authorized);
+      })
+    });
   }
 
 }
